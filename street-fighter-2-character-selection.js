87cb36f1a2a928c8072abe2122b6e5c4096fe375
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-14 18:30:39 -0600

    1st coommitt

---
+++ street-fighter-2-character-selection.js
@@ -0,0 +1,12 @@
+fighters = [
+	["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
+	["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"]
+];
+
+initial_position = (0,0)
+moves = ['up', 'left', 'right', 'left', 'left'//->['Ryu', 'Vega', 'Ryu', 'Vega', 'Balrog']
+
+
+function streetFighterSelection(fighters, position, moves){
+  return "";
+}
